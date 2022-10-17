0e212165fdcb2110b66ca34f1cecf191e48e8c55
Josiah Viernes <viernesjosiah6@gmail.com>
2022-10-17 10:02:43 +0800

    add

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const write = require('./write')
 const read = require('./read')
+const add = require('./add')
 
-const cmd = process.argv[2]
+const cmd = process.argv
 
-write(cmd)
+//write()
 
-console.log(read())
+//console.log(read())
+
+var note = {}
+
+if(cmd[2] == 'add') {
+    // Build Object
+    note = { 
+        id: cmd[3],
+        name: cmd[4],
+        gender: cmd[5]
+    }
+    // Get old note value
+    var oldNote = read()
+    // Add note to note.txt
+    add(note, oldNote)
+}
+
+if(cmd[2] == 'read') {
+    // Import present
+    const present = require('./present')
+    
+    present(read())
+}
